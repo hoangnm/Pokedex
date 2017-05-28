fa8f2120bc89c9adadf11903cead09d9b8d00912
hoang.nguyen <hoang.nguyen@pyramid-consulting.com>
2017-05-28 11:17:11 +0700

    Initial Commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Hoang Nguyen Minh on 5/28/17.
+//  Copyright © 2017 Hoang Nguyen Minh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
